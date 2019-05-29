1270933b453406b62ec33adf704666e521a9a958
Reed Law <reed@doctible.com>
2019-05-29 10:51:24 -0700

    debug

--- lib/webpacker/runner.rb
+++ lib/webpacker/runner.rb
@@ -9,9 +9,13 @@ module Webpacker
     def initialize(argv)
       @argv = argv
 
+      $stderr.puts "*"*100
       @app_path              = File.expand_path(".", Dir.pwd)
+      $stderr.puts @app_path
       @node_modules_bin_path = ENV["WEBPACKER_NODE_MODULES_BIN_PATH"] || `yarn bin`.chomp
+      $stderr.puts @node_modules_bin_path
       @webpack_config        = File.join(@app_path, "config/webpack/#{ENV["NODE_ENV"]}.js")
+      $stderr.puts @webpack_config
 
       unless File.exist?(@webpack_config)
         $stderr.puts "webpack config #{@webpack_config} not found, please run 'bundle exec rails webpacker:install' to install Webpacker with default configs or add the missing config file for your custom environment."
